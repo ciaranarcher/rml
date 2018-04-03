af0cc98a8cc00e11f636a85e9a0e5a05316105a0
Ciaran Archer <carcher@zendesk.com>
2018-04-03 16:44:05 -0700

    Initial commit.

---
+++ item.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Item
+  attr_accessor :type, :count, :value
+end
---
+++ spec/item_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+require_relative '../item'
+
+describe 'something' do
+  it 'should work' do
+    assert Item.new
+  end
+end
